88a71cdfe6751b00d75ac18b83008049d130a66c
ronseriajorge <ronseriajorge@gmail.com>
2024-11-15 10:31:37 -0500

    Se ajusta el tema de CORS en las api de usuarios, inventarios y apigateway

--- api_gateway/app/main.py
+++ api_gateway/app/main.py
@@ -1,7 +1,17 @@
 from fastapi import FastAPI
 from app.api.routes import router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+# Configuración de CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Permitir todas las solicitudes. Cambiar a dominios específicos en producción.
+    allow_credentials=True,  # Permitir envío de cookies o headers como Authorization
+    allow_methods=["*"],  # Permitir todos los métodos HTTP (GET, POST, PUT, DELETE, etc.)
+    allow_headers=["*"],  # Permitir todos los headers
+)
 
 # Registrar el enrutador principal
 app.include_router(router)
--- inventario_ms/main.py
+++ inventario_ms/main.py
@@ -1,7 +1,17 @@
 from fastapi import FastAPI
 from app.routers import inventario, transaccion
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+# Configuración de CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Permitir todas las solicitudes. Cambiar a dominios específicos en producción.
+    allow_credentials=True,  # Permitir envío de cookies o headers como Authorization
+    allow_methods=["*"],  # Permitir todos los métodos HTTP (GET, POST, PUT, DELETE, etc.)
+    allow_headers=["*"],  # Permitir todos los headers
+)
+
 
 app.include_router(inventario.router, prefix="/api")
 app.include_router(transaccion.router, prefix="/api")
